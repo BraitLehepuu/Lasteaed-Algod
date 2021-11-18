b6b6202f4b9cefd960fabcb0fa3e3a2abb462d58
RonaldJudin <ronaljud@gmail.com>
2021-11-18 19:35:27 +0200

    Kirjeldasin Quicksorti põhimõtted ära, pean veel tegema liidese. See on väga raske.

--- Algod/Quicksort/Algo2.js
+++ Algod/Quicksort/Algo2.js
@@ -3,7 +3,7 @@
 const gridWidth = 10;      // Ruudustiku laius. Ma pean seda proovima paljude erinevate numbritega.
 const gridHeight = 1;      // Ruudustiku kõrgus
 var grid = NaN;            // Ruudustik
-var speed = 100;
+var speed = 200;
 
 // Funktsioon algsete olude seadmiseks
 function SetUp(){
@@ -17,10 +17,10 @@ function DisplayGrid(){
 
     // Teeb iga ruudu jaoks ühe HTML sõne
     var grid_html = []
-    for(var j = 0; j < gridHeight; j++){
+    for(var j = 0; j = gridHeight; j++){
         grid_html.push('<div class="grid_column">');
         for(var i = 0; i < gridWidth; i++){
-            len = grid_html.push('<div class="tile tile_empty" id="' + i + '_' + j + '" onclick="TileClicked(this.id)"><h1>' + i + ', ' + j + '</h1></div>'); // Nimetab ruudud
+            len = grid_html.push('<div class="tile tile_empty" id="' + i + '</h1></div>'); // Nimetab ruudud
         }
         grid[i][j] = false;
     }
@@ -50,7 +50,7 @@ function OnSpeedChanged(value){
 // --------------------------------------------------------------------------------------------------------------------------------------------------
 var currentlySolving = false; // Algoritmi tööolek
 
-// Alusta algoritmiga
+// Alustab algoritmiga
 function StartQuicksort(){
     currentlySolving = true;
     Quicksort
@@ -61,30 +61,32 @@ function StartQuicksort(){
 async function Quicksort(){
 
     // Algsed muutujad ja nende väärtused
-    const rida = []; // Siia me lisame sorteeritavad arvud.
-    for(var len = 0; length < gridWidth; len++){ // Genereerime arvud juhuslikult.
+    var queue = []; // Selle ma pean ümber tegema.
+    var rida = []; // Siia me lisame sorteeritavad arvud.
+    for(var arv = 0; arv < gridWidth; arv++){ // Genereerime arvud juhuslikult.
         rida.push(Math.floor(Math.random() * 100));
     }
     var pivot = Math.floor(rida.length/2);
+    AddToQueue(pivot, queue);
     var pointer1 = 0;
+    AddToQueue(pointer1, queue);
     var pointer2 = rida.length-1; // Ma ei salli nende semikoolonite rohkust kohe üldse.
+    AddToQueue(pointer2, queue);
     Sort(rida, pivot, pointer1, pointer2);
-} // Sorteerimisfunktsioon on rekursiivne, aga selle päises ei tohiks uusi muutujaid deklareerida.
+} // Sorteerimisfunktsioon on rekursiivne, aga selle päises ei tohiks kogu aeg uusi muutujaid deklareerida.
 
 async function Sort(rida, pivot, pointer1, pointer2){ // Sorteerimine ise
     if(pointer1 >= pointer2){ // Sel juhul on sorteerimine selleks korraks otsas. Jätkub rekursioon, need tulemused pannakse uuesti kokku.
+        EmptyQueue();
         const uusrida = rida.slice(pivot+1);
         const teinerida = rida.slice(0, pivot+1);
-        if(uusrida!=[]){
+        if(uusrida.length>1){
             Sort(uusrida, Math.floor(uusrida.length/2), 0, uusrida.length-1);
         }
-        if(teinerida!=[]){
+        if(teinerida.length>1){
             Sort(teinerida, Math.floor(teinerida.length/2), 0, teinerida.length-1);
         }
-        const valmisrida = uusrida.concat(teinerida)
-        console.log(valmisrida)
-
-
+        const valmisrida = uusrida.concat(teinerida);
     }
 
     if(rida[pointer1]>pivot && pivot>rida[pointer2]){ // Sel juhul need vahetatakse. See on kogu sorteerimise alus.
@@ -92,10 +94,60 @@ async function Sort(rida, pivot, pointer1, pointer2){ // Sorteerimine ise
         rida[pointer2] = rida [pointer1];
         rida[pointer1] = žonglöör;
         pointer1 = pointer1+1;
+        AddToQueue(pointer1, queue);
         pointer2 = pointer2-1;
+        AddToQueue(pointer2, queue);
         Sort(rida, pivot, pointer1, pointer2);
-    } else { // Ja muidu ta neid ei vaheta, liigub lihtsalt edasi.
+    } else if(pivot<rida[pointer2]){ // Ja muidu ta neid ei vaheta, liigub lihtsalt edasi.
         pointer2 = pointer2-1;
+        AddToQueue(pointer2, queue);
+        if(rida[pointer1]<pivot){
+            pointer1 = pointer1+1
+            AddToQueue(pointer1, queue);
+        }
         Sort(rida, pivot, pointer1, pointer2);
         }
+}
+
+var visualQueue = []            // Järjend visuaalse järjekorra jaoks - Ma pean endalt küsima, kas seda on ka vaja.
+
+// Lisab ruudu visuaalsesse järjekorda ning näita
+async function AddToQueue(element, queue){
+    queue.push(element); // Lisa element algoritmi järjekorda
+    visualQueue.push('<div class="queue_item queue_unexplored"><h1></h1><p></p></div>'); // Lisa element visuaalsesse järjekorda - Ma pean seda veel harjutama, see ei ole õige.
+
+    RefreshQueue();
+
+    // Värvib ruudu reas, kui see on pivot ja/või pointer.
+    for(var k=0; k<queue.length; k++){
+        if(queue[k]=pivot){
+        var tile = document.getElementById(element[0]);
+        tile.style.backgroundColor = "rgb(30,0,140)";
+        }
+        if(queue[k]==pointer1 || queue[k]==pointer2){
+            var tile = document.getElementById(element[0]);
+            tile.style.borderBlockColor = "rgb(140,0,30)";
+        }
+    }
+
+    await new Promise(r => setTimeout(r, speed));
+}
+
+// Tühjendab visuaalse järjekorra
+function EmptyQueue(){
+    visualQueue = []
+    RefreshQueue();
+}
+
+// Uuendab visuaalset järjekorda ekraanil
+function RefreshQueue(){
+    var queue_container = document.getElementById("queue_container");
+    queue_container.innerHTML = "";
+}
+
+// Viib algoritmi ja rea algstaadiumisse
+function ResetQuicksort(){
+    currentlySolving = false;
+    EmptyQueue();
+    SetUp();
 }
